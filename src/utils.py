2a3ebf018d2272408976e1d14d254d137e39049b
Mohammad Alkhalifah <56599163+Mkhlf@users.noreply.github.com>
2024-07-10 13:10:52 +0300

    skeleton code for main loops and util file

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import torch
+import os 
+import matplotlib.pyplot as plt
 import numpy as np
-from skimage import io, transform
-from torchvision import transforms
-from skimage.transform import AffineTransform, warp
-
-# Mean and standard deviation for normalization
-imagenet_mean = np.array([0.485, 0.456, 0.406])
-imagenet_std = np.array([0.229, 0.224, 0.225])
+from dataset import SegyDataset, NpyDataset
+from models import *
+from metrics import *
+from loss import *
+import torch.utils.data.dataloader as DataLoader
+import json
+import warnings
 
 def dict_without_key(d, key):
     """
@@ -23,152 +25,6 @@ def dict_without_key(d, key):
     new_d.pop(key)
     return new_d
 
-class RandomHorizontalFlip():
-    def __init__(self, prob=0.5):
-        """
-        Initialize the RandomHorizontalFlip transform.
-
-        Args:
-            prob (float): Probability of flipping the image. Defaults to 0.5.
-        """
-        self.prob = prob
-
-    def __call__(self, sample):
-        """
-        Apply the transform to the sample.
-
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
-
-        Returns:
-            dict: The transformed sample.
-        """
-        x = sample['input']
-        y = sample['target']
-        if np.random.rand(1) < self.prob:
-            sample['input'] = torch.flip(x, dims=(-1,))
-            sample['target'] = torch.flip(y, dims=(-1,))
-        return sample
-
-class ScaleNormalize():
-    def __init__(self, type='input'):
-        """
-        Initialize the ScaleNormalize transform.
-
-        Args:
-            type (str): Type of normalization ('input' or 'target'). Defaults to 'input'.
-        """
-        self.type = type
-
-    def __call__(self, sample):
-        """
-        Apply the transform to the sample.
-
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
-
-        Returns:
-            dict: The transformed sample.
-        """
-        x = sample[self.type]
-        if x.dtype == int:
-            return sample
-        x /= np.abs(x).max()
-        return sample
-
-class RandomShift():
-    def __init__(self, mean=1, std=1e-5, shift_mean=0, shift_std=1e-3):
-        """
-        Initialize the RandomShift transform.
-
-        Args:
-            mean (float): Mean value for scaling. Defaults to 1.
-            std (float): Standard deviation for scaling. Defaults to 1e-5.
-            shift_mean (float): Mean value for shifting. Defaults to 0.
-            shift_std (float): Standard deviation for shifting. Defaults to 1e-3.
-        """
-        self.mean = mean
-        self.std = std
-        self.shift_mean = shift_mean
-        self.shift_std = shift_std
-
-    def __call__(self, sample):
-        """
-        Apply the transform to the sample.
-
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
-
-        Returns:
-            dict: The transformed sample.
-        """
-        image, target = sample['input'], sample['target']
-        wx, wy = image.shape[:2]
-        scale = np.random.uniform(low=self.mean - np.sqrt(3) * self.std, high=self.mean + np.sqrt(3) * self.std)
-        shift = scale * np.random.uniform(low=self.shift_mean - np.sqrt(3) * self.shift_std, high=self.shift_mean + np.sqrt(3) * self.shift_std)
-        scaling = AffineTransform(scale=(1, scale), translation=(0, wy - scale*wy - 1))
-        scaled = warp(image, scaling, mode='constant', preserve_range=True)
-        scaled_target = warp(target, scaling, mode='constant', preserve_range=True)
-        shift = AffineTransform(translation=(0, shift))
-        shifted = warp(scaled, shift, mode='edge', preserve_range=True)
-        shifted_target = warp(scaled_target, shift, mode='edge', preserve_range=True)
-        sample['input'] = shifted.astype(image.dtype)
-        sample['target'] = shifted_target.astype(target.dtype)
-        return sample
-
-class BaseNormalize():
-    def __init__(self, mean, std):
-        """
-        Initialize the BaseNormalize transform.
-
-        Args:
-            mean (array-like): Mean values for normalization.
-            std (array-like): Standard deviation values for normalization.
-        """
-        self.normalize = transforms.Normalize(mean, std)
-
-    def __call__(self, x):
-        """
-        Apply the normalization to the input.
-
-        Args:
-            x (Tensor): Input tensor.
-
-        Returns:
-            Tensor: Normalized tensor.
-        """
-        return self.normalize(x)
-
-class InputNormalize(BaseNormalize):
-    def __call__(self, sample):
-        """
-        Apply the normalization to the input sample.
-
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
-
-        Returns:
-            dict: The transformed sample with normalized input.
-        """
-        x = sample['input']
-        sample['input'] = super().__call__(x)
-        return sample
-
-class TargetNormalize(BaseNormalize):
-    def __call__(self, sample):
-        """
-        Apply the normalization to the target sample.
-
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
-
-        Returns:
-            dict: The transformed sample with normalized target.
-        """
-        x = sample['target']
-        sample['target'] = super().__call__(x)
-        return sample
-
 class Clamp():
     def __call__(self, sample):
         """
@@ -183,7 +39,7 @@ class Clamp():
         sample['input'] = torch.clamp(sample['input'], 0.0, 1.0)
         sample['target'] = torch.clamp(sample['target'], 0.0, 1.0)
         return sample
-
+    
 class ToTensor(object):
     def __call__(self, sample):
         """
@@ -198,85 +54,31 @@ class ToTensor(object):
         input, target = sample['input'], sample['target']
         return {'input': torch.from_numpy(input), 'target': torch.from_numpy(target)}
 
-class FlipChannels(object):
-    def __init__(self, only_input=False):
+class Scale():
+    def __init__(self, problem='regr'):
         """
-        Initialize the FlipChannels transform.
+        Initialize the Scale transform.
 
         Args:
-            only_input (bool): Whether to flip only the input channels. Defaults to False.
+            problem (str): The problem type ('regr' for regression, 'segment' for segmentation). Defaults to 'regr'.
         """
-        self.only_input = only_input
+        self.problem = problem
 
     def __call__(self, sample):
         """
-        Flip the channels of the input (and optionally the target).
+        Scale the input and target values to the range [0, 1].
 
         Args:
             sample (dict): A sample containing 'input' and 'target'.
 
         Returns:
-            dict: The transformed sample with flipped channels.
+            dict: The transformed sample with scaled input and target.
         """
-        input, target = sample['input'], sample['target']
-        input = input.transpose((2, 0, 1))
-        if not self.only_input:
-            target = target.transpose((2, 0, 1))
-        return {'input': input, 'target': target}
-
-class Resize():
-    def __init__(self, target_size=(256, 256)):
-        """
-        Initialize the Resize transform.
-
-        Args:
-            target_size (tuple): The target size for resizing. Defaults to (256, 256).
-        """
-        self.target_size = target_size
-
-    def __call__(self, sample):
-        """
-        Resize the input and target to the target size.
-
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
-
-        Returns:
-            dict: The transformed sample with resized input and target.
-        """
-        sample['input'] = transform.resize(sample['input'], self.target_size, preserve_range=True)
-        sample['target'] = transform.resize(sample['target'], self.target_size, preserve_range=True)
+        sample['input'] = sample['input'] / 255.
+        if self.problem == 'regr':
+            sample['target'] = sample['target'] / 255.
         return sample
-
-class MinResize(Resize):
-    def __init__(self, min_size=256):
-        """
-        Initialize the MinResize transform.
-
-        Args:
-            min_size (int): The minimum size for resizing. Defaults to 256.
-        """
-        self.min_size = min_size
-        super().__init__()
-
-    def __call__(self, sample):
-        """
-        Resize the input and target to ensure the minimum size.
-
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
-
-        Returns:
-            dict: The transformed sample with resized input and target.
-        """
-        wx0, wy0, _ = sample['input'].shape
-        min_dim = min(wx0, wy0)
-        k = 1
-        if min_dim < self.min_size:
-            k = self.min_size / min_dim
-        self.target_size = k * np.array((wx0, wy0))
-        return super().__call__(sample)
-
+    
 class ChangeType():
     def __init__(self, problem='regr'):
         """
@@ -303,65 +105,177 @@ class ChangeType():
         else:
             sample['target'] = sample['target'].astype(np.int)
         return sample
+    
+def plot_losses(train_losses, val_losses, fname, scale='linear', title='Loss per Epoch'):
+    """
+    Plots the training and validation losses across epochs and saves the plot as an image file with name - fname(function argument). 
 
-class Scale():
-    def __init__(self, problem='regr'):
-        """
-        Initialize the Scale transform.
+    Args:
+        train_losses (list): List of training losses for each epoch.
+        val_losses (list): List of validation losses for each epoch.
+        fname (str): Name of the file to save the plot (without extension).
 
-        Args:
-            problem (str): The problem type ('regr' for regression, 'segment' for segmentation). Defaults to 'regr'.
-        """
-        self.problem = problem
+    Returns:
+        None
+    """
 
-    def __call__(self, sample):
-        """
-        Scale the input and target values to the range [0, 1].
+	# Create 'plots' directory if it doesn't exist
+    if not os.path.isdir('plots'):
+        os.mkdir('plots')
 
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
+    # Plotting training and validation losses
+    plt.plot(train_losses, label='Training Loss')
+    plt.plot(val_losses, label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title(title)
+    plt.legend()
+    
+    # range of y-axis values
+    if scale == 'log':
+        plt.yscale('log')
+    else:
+        plt.yscale('linear')
 
-        Returns:
-            dict: The transformed sample with scaled input and target.
-        """
-        sample['input'] = sample['input'] / 255.
-        if self.problem == 'regr':
-            sample['target'] = sample['target'] / 255.
-        return sample
+    # Saving the plot as an image file in 'plots' directory
+    plt.savefig("./plots/" + fname + ".png")
+    
+def get_model (model, config):
+    """
+    Given a model name and a configuration dictionary, return the model object.
+    """
+    valid_models = []
+    assert model in valid_models, f"Invalid model name. Choose from {valid_models}"
+    
+    # TODO: Initialize the model and return it
+    pass
 
-class RandomCrop():
-    def __init__(self, target_size=(224, 224), edge=5):
-        """
-        Initialize the RandomCrop transform.
+def train_config(model, config_file):
+    """
+    Given a configuration file, return the configuration dictionary.
+    """
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+        
+    loss = get_loss(config)
+    opt = get_optimizer(model, config)
+    sch = get_scheduler(opt, config)
+    metrics = get_metrics(config)
+    (td, tl) = get_transforms(config)
+    
+    # if epochs is not specified
+    if 'epochs' not in config:
+        warnings.warn("Number of epochs not specified. Defaulting to 10.")
+        epochs = 10
+    else:
+        epochs = config['epochs']
+    
+    return loss, epochs, opt, sch, metrics, (td, tl)
 
-        Args:
-            target_size (tuple): The target size for the crop. Defaults to (224, 224).
-            edge (int): The edge padding for the crop. Defaults to 5.
-        """
-        self.target_size = target_size
-        self.edge = edge
+def get_loss(config):
+    """
+    Given a configuration dictionary, return the loss function.
+    """
+    
+    valid_losses = []
+    assert config['loss'] in valid_losses, f"Invalid loss name. Choose from {valid_losses}"
+    
+    # TODO: Initialize the loss function and return it
+    pass
 
-    def __call__(self, sample):
-        """
-        Randomly crop the input and target to the target size.
+def get_optimizer(model, config):
+    """
+    Given a model and a configuration dictionary, return the optimizer.
+    """
+    valid_optimizers = []
+    assert config['optimizer'] in valid_optimizers, f"Invalid optimizer name. Choose from {valid_optimizers}"
+    
+    # TODO: Initialize the optimizer and return it
+    pass
 
-        Args:
-            sample (dict): A sample containing 'input' and 'target'.
+def get_scheduler(optimizer, config):
+    """
+    Given an optimizer and a configuration dictionary, return the scheduler.
+    """
+    valid_schedulers = []
+    assert config['scheduler'] in valid_schedulers, f"Invalid scheduler name. Choose from {valid_schedulers}"
+    
+    # TODO: Initialize the scheduler and return it
+    pass
 
-        Returns:
-            dict: The transformed sample with randomly cropped input and target.
-        """
-        wx, wy = self.target_size
-        wx0, wy0, _ = sample['target'].shape
-        try:
-            center_x = np.random.randint(self.edge + wx // 2, wx0 - self.edge - wx // 2)
-            center_y = np.random.randint(self.edge + wy // 2, wy0 - self.edge - wy // 2)
-        except:
-            raise ValueError('Error in cropping: invalid sample shape', sample['target'].shape)
-        crop_x_0 = center_x - wx // 2
-        crop_x_1 = center_x + wx // 2
-        crop_y_0 = center_y - wy // 2
-        crop_y_1 = center_y + wy // 2
-        sample['input'] = sample['input'][crop_x_0:crop_x_1, crop_y_0:crop_y_1]
-        sample['target'] = sample['target'][crop_x_0:crop_x_1, crop_y_0:crop_y_1]
-        return sample
+def get_metrics(config):
+    """
+    Given a configuration dictionary, return the metrics.
+    """
+    valid_metrics = []
+    # check if all the metrics are valid
+    for metric in config['metrics']:
+        assert metric in valid_metrics, f"Invalid metric name. Choose from {valid_metrics}"
+    
+    # TODO: Initialize the metrics and return them
+    pass
+
+def get_transforms(config):
+    """
+    Given a configuration dictionary, return the data transformations and label transformations.
+    """
+    valid_transforms = []
+    # check if all the transforms are valid
+    for transform in config['transforms']:
+        assert transform in valid_transforms, f"Invalid transform name. Choose from {valid_transforms}"
+            
+    
+def get_loaders(type, config, td, tl, **kwargs):
+    """
+    Given a configuration file, load the dataset and return a train and validation DataLoader object.
+    """
+    with open(config, 'r') as f:
+        config = json.load(f)
+    
+    if type == 'segy':
+        return create_segy_loader(config, td, tl, **kwargs)
+    elif type == 'npy':
+        return create_npy_loader(config, td, tl, **kwargs)
+    
+    
+def create_segy_loader(config, td ,tl, **kwargs):
+    """
+    Given a configuration dictionary, load the segy dataset and return the DataLoader object.
+    """
+    train_dataset = SegyDataset.from_config(config['train'])
+    val_dataset = SegyDataset.from_config(config['val'])
+    
+    train_dataset.add_transforms(td, tl)
+    val_dataset.add_transforms(td, tl)
+    
+    collate_train = train_dataset.create_collate_fn(config['collate'], **kwargs)
+    collate_val = val_dataset.create_collate_fn(config['collate'], **kwargs)
+    
+    batch_size = config['batch_size']
+    num_workers = config['num_workers']
+    
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_train, num_workers=num_workers)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_val, num_workers=num_workers)
+    
+    return train_dataloader, val_dataloader
+
+def create_npy_loader(config, td, tl, **kwargs):
+    """
+    Given a configuration dictionary, load the npy dataset and return the DataLoader object.
+    """
+    train_dataset = NpyDataset.from_config(config['train'])
+    val_dataset = NpyDataset.from_config(config['val'])
+    
+    train_dataset.add_transforms(td, tl)
+    val_dataset.add_transforms(td, tl)
+    
+    collate_train = train_dataset.create_collate_fn(config['collate'], **kwargs)
+    collate_val = val_dataset.create_collate_fn(config['collate'], **kwargs)
+    
+    batch_size = config['batch_size']
+    num_workers = config['num_workers']
+    
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_train, num_workers=num_workers)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_val, num_workers=num_workers)
+    
+    return train_dataloader, val_dataloader
